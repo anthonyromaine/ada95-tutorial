e6d55381a979663a5023d013640be841c5a23794
Anthony Romaine <61034923+anthonyromaine@users.noreply.github.com>
2023-08-21 12:03:13 -0600

    Initialize Project and Add Introduction

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Docs',
+			sidebar: [
+				{
+					label: 'Ada 95 Tutorial',
+					autogenerate: { directory: 'ada95-tutorial'}
+				},
+			],
+		}),
+	],
+	redirects: {
+		'/': '/ada95-tutorial/part-1---beginning-ada-95-tutorial/introduction/',
+	},
+	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
